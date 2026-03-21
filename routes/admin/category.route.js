c78d58ec0c27786369d264b2f4a32789bb4095dd
lvh0301 <levanhieu0301@gmail.com>
2026-03-21 14:33:17 +0700

    validate bên backen(tạo danh mục)

--- routes/admin/category.route.js
+++ routes/admin/category.route.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const categoryController = require('../../controllers/admin/category.controller');
 const multer = require('multer');
 const cloudinary = require("../../helpers/cloudinary.helper")
-
+const categoryValidate =  require("../../validates/category.validate")
 const upload = multer({ storage: cloudinary.storage })
 
 router.get('/list',categoryController.list)
 router.get('/create',categoryController.create)
-router.post('/create', upload.single('avatar'), categoryController.createPost)
+router.post('/create', upload.single('avatar'),categoryValidate.categoryValidate ,categoryController.createPost)
 
 module.exports = router; 
---
+++ validates/category.validate.js
@@ -0,0 +1,32 @@
+const Joi = require('joi');
+
+
+module.exports.categoryValidate = (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string()
+      .required()
+      .email()
+      .messages({
+        "string.empty": "Vui lòng nhập tên danh mục",
+      }),
+    parent: Joi.string().allow(),
+    position: Joi.string().allow(),
+    status: Joi.string().allow(),
+    avatars: Joi.string().allow(),
+    description: Joi.string().allow(),
+  })
+  const { error } = schema.validate(req.body);
+
+  if(error) {
+    const errorMessage = error.details[0].message;
+
+    res.json({
+      code: "error",
+      message: errorMessage
+    });
+    return;
+  }
+  
+  next();
+ 
+}
