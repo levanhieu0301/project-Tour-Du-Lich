2f69f3364b3c31fd7410dbc19b873a73112cef20
lvh0301 <levanhieu0301@gmail.com>
2026-06-02 08:55:50 +0700

    Hiển thị data ra Section 4 (Trang chủ)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,6 @@
 const moment = require("moment");
 const Tour = require("../../models/tour.model");
+const Category = require("../../models/categories.model");
 
 module.exports.home = async (req, res) => {
   // Section 2 
@@ -18,9 +19,39 @@ module.exports.home = async (req, res) => {
       item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY");
     }
   // End Section 2
+    // Section 4: Tour Trong Nước
+  const categoryIdSection4 = "69be3a07bc3deb94c193b00a"; // Id của danh mục Tour Trong Nước
+  const listCategory = [categoryIdSection4];
+
+  const listSubCategory = await Category.find({
+    parent: categoryIdSection4,
+    deleted: false,
+    status: "active"
+  });
+
+  for (const item of listSubCategory) {
+    listCategory.push(item.id);
+  }
+  
+  const tourListSection4 = await Tour
+    .find({
+      category: { $in: listCategory },
+      deleted: false,
+      status: "active"
+    })
+    .sort({
+      positon: "desc"
+    })
+    .limit(8)
+
+  for(const item of tourListSection4) {
+    item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY");
+  }
+  // End Section 4: Tour Trong Nước
 
   res.render("client/pages/home", {
     pageTitle: "Trang chủ",
-    tourListSection2: tourListSection2
+    tourListSection2: tourListSection2,
+    tourListSection4: tourListSection4
   });
 }
