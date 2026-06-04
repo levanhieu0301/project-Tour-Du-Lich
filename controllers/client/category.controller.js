0f592256cad88c6b79fac0dc1c41cc6bd951c697
lvh0301 <levanhieu0301@gmail.com>
2026-06-04 08:40:15 +0700

    Hiển thị danh sách tour (trang Danh sách tour)

--- controllers/client/category.controller.js
+++ controllers/client/category.controller.js
@@ -1,5 +1,6 @@
 const Category = require("../../models/categories.model");
-
+const Tour = require("../../models/tour.model");
+const moment = require("moment");
 module.exports.list = async (req, res) => {
     const breadcrumb = {
       image: "",
@@ -43,9 +44,42 @@ module.exports.list = async (req, res) => {
 
 
   // End Breadcrumb
+  // lấy hết tour theo category
+  const listCategoryId = [];
+  const childCategory = async (parentId) => {
+    const childs = await Category.find({
+      parent: parentId,
+      deleted: false,
+      status: "active"
+    })
+    for(const child of childs) {
+      listCategoryId.push(child._id);
+
+      await childCategory(child._id);
+    }
+  };
+  await childCategory(category._id);
+
+  
+  const tourListSection9 = await Tour
+    .find({
+      category: { $in: [category.id, ...listCategoryId] },
+      status: "active",
+      deleted: false
+    })
+    .sort({
+      position: "desc"
+    })
+
+  for(const item of tourListSection9) {
+    item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY");
+  }
+
+  // End lấy hết tour theo category
   res.render("client/pages/tour-list", {
     pageTitle: "Danh sách tour",
     breadcrumb: breadcrumb,
-    categoryTitle: category
+    categoryTitle: category,
+    tourListSection9: tourListSection9
   })
 }
