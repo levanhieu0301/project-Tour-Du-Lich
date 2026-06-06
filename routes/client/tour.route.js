3dcdbd307cd23e8de2076d5daf292411884f620d
lvh0301 <levanhieu0301@gmail.com>
2026-06-06 09:01:40 +0700

    Làm khối Breadcrumb(trang chi tiết tour)

--- routes/client/tour.route.js
+++ routes/client/tour.route.js
@@ -1,7 +1,6 @@
 const router = require("express").Router()
 const tourListController = require("../../controllers/client/tour.controller")
 
-router.get('/', tourListController.tourList)
-router.get('/detail', tourListController.tourDetail)
+router.get('/detail/:slug', tourListController.tourDetail)
 
 module.exports = router;
